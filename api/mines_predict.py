0b6e2a7de0b4c9091ed4b69f554d3f392ad93185
SkunkyDev <skunk.biz2025@gmail.com>
2025-06-21 11:39:13 +0530

    Create mines_predict.py

---
+++ api/mines_predict.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, jsonify
+from stake_logic import generate_mines_layout
+
+app = Flask(__name__)
+
+@app.route("/api/mines_predict", methods=["POST"])
+def predict():
+    data = request.json
+    mines = data.get("mines", 3)
+    tiles = generate_mines_layout(mines)
+    return jsonify({"tiles": tiles})
